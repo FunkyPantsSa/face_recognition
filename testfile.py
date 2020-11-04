c292dc929c787471d29a823aab16a00f8fe4257b
FunkyPants <34020994+FunkyPantsSa@users.noreply.github.com>
2020-11-04 10:12:47 +0800

    用wxpython进行test

--- testfile.py
+++ testfile.py
@@ -1,5 +1,6 @@
 import threading
 import time
+import wx
 
 
 def run(n):
@@ -13,8 +14,34 @@ def run(n):
     time.sleep(1)
 
 
+# if __name__ == '__main__':
+#     t1 = threading.Thread(target=run, args=("t1",))
+#     t2 = threading.Thread(target=run, args=("t2",))
+#     t1.start()
+#     t2.start()
+# -*- coding:utf-8 -*-
+
+
+class Frame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, title='', size=(400, 300), name='frame', style=541072960)
+        self.启动窗口 = wx.Panel(self)
+        self.Centre()
+        self.按钮1 = wx.Button(self.启动窗口, size=(80, 32), pos=(269, 40), label='按钮', name='button')
+        self.按钮1.Bind(wx.EVT_BUTTON, self.按钮1_按钮被单击)
+
+    def 按钮1_按钮被单击(self, event):
+        from main import  camera
+        camera()
+
+
+class myApp(wx.App):
+    def OnInit(self):
+        self.frame = Frame()
+        self.frame.Show(True)
+        return True
+
+
 if __name__ == '__main__':
-    t1 = threading.Thread(target=run, args=("t1",))
-    t2 = threading.Thread(target=run, args=("t2",))
-    t1.start()
-    t2.start()
+    app = myApp()
+    app.MainLoop()
